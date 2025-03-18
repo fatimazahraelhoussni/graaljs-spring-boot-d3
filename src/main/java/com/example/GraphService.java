c59720af0143c1e5fb1a98ce8940e98dd645bb0b
fatimazahraelhoussni <fatimazahra.elhoussni20@ump.ac.ma>
2025-03-18 10:26:23 +0000

    Refactor GraphService to load graph.js using ClassPathResource

--- src/main/java/com/example/GraphService.java
+++ src/main/java/com/example/GraphService.java
@@ -3,7 +3,7 @@ package com.example;
 import org.graalvm.polyglot.*;
 import org.graalvm.polyglot.io.IOAccess;
 import org.springframework.stereotype.Service;
-
+import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,7 +14,7 @@ public class GraphService {
     private final Engine sharedEngine = Engine.newBuilder().option("engine.TraceCompilation", "true").build();
 
     public String generateGraph() throws IOException {
-        String jsCode = new String(Files.readAllBytes(Paths.get("src/main/resources/static/graph.js")));
+//        String jsCode = new String(Files.readAllBytes(Paths.get("src/main/resources/static/graph.js")));
 
         try (Context context = Context.newBuilder("js")
                 .engine(sharedEngine)
@@ -23,9 +23,10 @@ public class GraphService {
                 .option("js.commonjs-require", "true")
                 .build()) {
 
-            Value result = context.eval("js", jsCode);
-
-           return result.asString();
+//            Value result = context.eval("js", jsCode);
+            Source photonSource = Source.newBuilder("js", new ClassPathResource("static/graph.js").getURL()).mimeType("application/javascript+module").build();
+            Value result = context.eval(photonSource);
+            return result.asString();
 
         } catch (PolyglotException e) {
             System.err.println("JavaScript Error: " + e.getMessage());
